6e37044fa7a2a7fc4758ca66af3258762bd1712f
rickymwethera <rickymwethera@gmail.com>
2020-09-11 01:52:59 +0300

    added models

---
+++ config.py
@@ -0,0 +1,7 @@
+API = ['12ac3b30fdfe4c479c98a7b8ccf3968b']
+
+APPLE_NEWS = ['http://newsapi.org/v2/everything?q=apple&from=2020-09-09&to=2020-09-09&sortBy=popularity&apiKey=12ac3b30fdfe4c479c98a7b8ccf3968b']
+
+WALLS = ['http://newsapi.org/v2/everything?domains=wsj.com&apiKey=12ac3b30fdfe4c479c98a7b8ccf3968b']
+
+BTC = ['http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-10&sortBy=publishedAt&apiKey=12ac3b30fdfe4c479c98a7b8ccf3968b']
