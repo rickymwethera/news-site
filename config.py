9dae2655c06b828d26adaec71a1fad4cbf555337
rickymwethera <rickymwethera@gmail.com>
2020-09-16 01:30:20 +0300

    added api key

--- config.py
+++ config.py
@@ -1,7 +1,34 @@
-API = ['12ac3b30fdfe4c479c98a7b8ccf3968b']
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLE_URL='https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    TOP_HEADLINES_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    NEWS_API_KEY = '12ac3b30fdfe4c479c98a7b8ccf3968b'
 
-APPLE_NEWS = ['http://newsapi.org/v2/everything?q=apple&from=2020-09-09&to=2020-09-09&sortBy=popularity&apiKey=12ac3b30fdfe4c479c98a7b8ccf3968b']
 
-WALLS = ['http://newsapi.org/v2/everything?domains=wsj.com&apiKey=12ac3b30fdfe4c479c98a7b8ccf3968b']
 
-BTC = ['http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-10&sortBy=publishedAt&apiKey=12ac3b30fdfe4c479c98a7b8ccf3968b']
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
