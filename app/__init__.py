682d790c013f25db347d042351f277fe006c71c5
rickymwethera <rickymwethera@gmail.com>
2020-09-16 01:27:28 +0300

    added config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from config import DevConfig
+# from flask_bootstrap import Bootstrap
 
 #initializing app
 
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
+
+#config
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
 
 from app import views
